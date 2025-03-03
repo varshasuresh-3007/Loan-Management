f3a0feb9cd8bc11783fc2195e9c98305aaed1fed
varshasuresh-3007 <varshasuresh3001@gmail.com>
2025-03-03 10:39:26 +0530

    Updated database settings and reconfigured environment variables

--- loan_management/settings.py
+++ loan_management/settings.py
@@ -195,7 +195,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
